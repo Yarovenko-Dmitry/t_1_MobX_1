0f7a8a814872fd3be6b7cb5da542101cc8d8f4c3
dmitry <yarovenkojob@gmail.com>
2023-03-23 15:23:58 +0300

    feat: add mobx counter

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,16 +1,21 @@
 import React, {FC} from 'react';
+import './App.css';
 
-export const Counter: FC = () => {
+import {counter} from "./store/counter";
+import {observer} from "mobx-react-lite";
+
+export const Counter: FC = observer(() => {
     return (
         <div className='counter'>
+            <div> {`Counter : ${counter.count}`}</div>
             <div className='buttons'
             >
-                <button className='btn'>+</button>
-                <button className='btn'>-</button>
+                <button className='btn' onClick={() => counter.increment()}>+</button>
+                <button className='btn' onClick={() => counter.decrement()}>-</button>
             </div>
         </div>
     )
-}
+})
 
 
 
