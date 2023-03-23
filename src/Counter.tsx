edf00d432f157d28133d4ba6bae52208d0243d49
dmitry <yarovenkojob@gmail.com>
2023-03-23 14:45:41 +0300

    feat: add UI

---
+++ src/Counter.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+
+export const Counter: FC = () => {
+    return (
+        <div className='counter'>
+            <div className='buttons'
+            >
+                <button className='btn'>+</button>
+                <button className='btn'>-</button>
+            </div>
+        </div>
+    )
+}
+
+
+
