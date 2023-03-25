ecea7770081ed82c3190aa8654ea6cb84df24568
dmitry <yarovenkojob@gmail.com>
2023-03-25 16:46:32 +0300

    fix: add other standard settings in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,31 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     es2021: true,
   },
   extends: [
+    'airbnb',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    // 'plugin:react-hooks/recommended', // causing problems, replaced by 'airbnb'
     'plugin:react/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
-    'standard-with-typescript',
+    // 'standard-with-typescript', // causing problems, replaced by 'airbnb/hooks'
+    // Prettier always last
     'plugin:prettier/recommended',
   ],
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 2020,
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'require-jsdoc': 'off',
